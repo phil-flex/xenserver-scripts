f7ad4ba9cd8769d704382f028f1c619aa81de370
phil <phil@flexsystem.com>
2018-11-12 16:34:02 +0800

    * update the command order to prevent xe command removed the mount point /mnt/iso

--- root/mount-iso.sh
+++ root/mount-iso.sh
@@ -3,7 +3,7 @@
 VG=`lvs |awk '/isoLibs/ {print $2}'`
 SR=`xe sr-list name-label=Remote\ ISO\ Library\ on:\ /mnt/iso |awk '/^uuid/ {print $5}'`
 PD=`xe pbd-list sr-uuid=$SR|awk '/^uuid/ {print $5}'`
-vgchange -ay $VG --config global{metadata_read_only=0}
-mount /dev/$VG/isoLibs /mnt/iso
 xe pbd-unplug uuid=$PD
 xe pbd-plug uuid=$PD
+vgchange -ay $VG --config global{metadata_read_only=0}
+mount /dev/$VG/isoLibs /mnt/iso
